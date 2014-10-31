f314df40d66e91a4d1c8bf7e2dd568c8282fe2b9
Jeff Frazier <jefff@knowclassic.com>
2014-10-31 00:49:25 -0400

    Initial commit

---
+++ sites/all/themes/smallwoodnickle/js/smallwood.js
@@ -0,0 +1,27 @@
+(function ($) {
+	Drupal.behaviors.smallwood = {
+   	attach:function() {
+
+   		/* Toggle the visibility of the staff bios */
+			$('.team').click(function() {
+				var w = $(this).width(),
+				 	 h = $(this).height(),
+				    bio = $('.bio',this),
+				    bro = $(this).siblings(),
+					 cont = $('.masonry');
+
+
+				$(this).removeClass('faded').toggleClass('active').siblings().next('.bio.masonry-item').remove();
+
+				if ($('.bio.masonry-item').length == 0) {
+					$(bio).clone().toggleClass('masonry-item').css({'left':w+10, 'width':w, 'height':h}).insertAfter(this);
+				}
+				else {
+					$('.bio.masonry-item').remove()
+				}
+				
+				cont.masonry('reload');
+			});
+		}
+	}
+}) (jQuery);
