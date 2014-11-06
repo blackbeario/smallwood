52a18f23b87e09ab791541c8c6456a23f3392093
Jeff Frazier <jefff@knowclassic.com>
2014-11-06 15:52:08 -0500

    Fix masonry fading

--- sites/all/themes/smallwoodnickle/js/smallwood.js
+++ sites/all/themes/smallwoodnickle/js/smallwood.js
@@ -2,7 +2,7 @@
 	Drupal.behaviors.smallwood = {
    	attach:function() {
 
-   		/**
+   	/**
       * Toggle the visibility of the staff bios.
       */
 			$('.team').click(function() {
@@ -11,18 +11,21 @@
 				    bio = $('.bio',this),
 				    bro = $(this).siblings(),
 				    cont = $('.masonry');
+				    $(this).removeClass('faded');
+
         
         // Remove all appended bios from sibling masonry elements.
-				$(this).siblings().next('.bio.masonry-item').remove();
+				bro.addClass('faded').next('.bio.masonry-item').remove();
 
         // Clone the staff bio and insert after clicked staff with masonry class.
-				if ($('.bio.masonry-item').length == 0) {
-					$(bio).clone().toggleClass('masonry-item').css({'left':w+10, 'width':w, 'height':h}).insertAfter(this);
+				if ($('.bio.masonry-item').length === 0) {
+					bio.clone().toggleClass('masonry-item').css({'left':w+10, 'width':w, 'height':h}).insertAfter(this);
 				}
         // Remove bio on second click if already inserted.
 				else {
-					$('.bio.masonry-item').remove()
-				}
+					$('.bio.masonry-item').remove();
+						bro.removeClass('faded');
+					}
 				// Reinit the masonry function to re-order the items.
 				cont.masonry('reload');
 			});
