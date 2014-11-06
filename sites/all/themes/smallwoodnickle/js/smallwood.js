1295de5a35457822d9f9c2c711d97d943d3ad29e
Jeff Frazier <jefff@knowclassic.com>
2014-11-06 15:54:32 -0500

    Convert tabs to spaces

--- sites/all/themes/smallwoodnickle/js/smallwood.js
+++ sites/all/themes/smallwoodnickle/js/smallwood.js
@@ -1,34 +1,34 @@
 (function ($) {
-	Drupal.behaviors.smallwood = {
-   	attach:function() {
+   Drupal.behaviors.smallwood = {
+      attach:function() {
 
-   	/**
+      /**
       * Toggle the visibility of the staff bios.
       */
-			$('.team').click(function() {
-				var w = $(this).width(),
-				    h = $(this).height(),
-				    bio = $('.bio',this),
-				    bro = $(this).siblings(),
-				    cont = $('.masonry');
-				    $(this).removeClass('faded');
+         $('.team').click(function() {
+            var w = $(this).width(),
+                h = $(this).height(),
+                bio = $('.bio',this),
+                bro = $(this).siblings(),
+                cont = $('.masonry');
+                $(this).removeClass('faded');
 
         
         // Remove all appended bios from sibling masonry elements.
-				bro.addClass('faded').next('.bio.masonry-item').remove();
+            bro.addClass('faded').next('.bio.masonry-item').remove();
 
         // Clone the staff bio and insert after clicked staff with masonry class.
-				if ($('.bio.masonry-item').length === 0) {
-					bio.clone().toggleClass('masonry-item').css({'left':w+10, 'width':w, 'height':h}).insertAfter(this);
-				}
+            if ($('.bio.masonry-item').length === 0) {
+               bio.clone().toggleClass('masonry-item').css({'left':w+10, 'width':w, 'height':h}).insertAfter(this);
+            }
         // Remove bio on second click if already inserted.
-				else {
-					$('.bio.masonry-item').remove();
-						bro.removeClass('faded');
-					}
-				// Reinit the masonry function to re-order the items.
-				cont.masonry('reload');
-			});
-		}
-	}
+            else {
+               $('.bio.masonry-item').remove();
+                  bro.removeClass('faded');
+               }
+            // Reinit the masonry function to re-order the items.
+            cont.masonry('reload');
+         });
+      }
+   }
 })(jQuery);
