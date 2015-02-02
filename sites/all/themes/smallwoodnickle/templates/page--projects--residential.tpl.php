8076d9b94a2da34ef4a286047690f2bfaffd5a09
Jeff Frazier <jefff@knowclassic.com>
2015-02-02 02:16:39 -0500

    Fix interior pages menu

--- sites/all/themes/smallwoodnickle/templates/page--projects--residential.tpl.php
+++ sites/all/themes/smallwoodnickle/templates/page--projects--residential.tpl.php
@@ -73,35 +73,18 @@
  * @ingroup themeable
  */
 ?>
-<header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
-  <div class="container">
-    <div class="navbar-header">
-      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-    </div>
-
-    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-      <div class="navbar-collapse collapse">
-        <nav role="navigation">
-          <?php if (!empty($primary_nav)): ?>
-            <?php print render($primary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($secondary_nav)): ?>
-            <?php print render($secondary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
-          <?php endif; ?>
-        </nav>
-      </div>
-    <?php endif; ?>
-  </div>
-</header>
+<nav class="nav sticky">
+  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+  <span class="sr-only">Toggle navigation</span>
+  <span class="icon-bar"></span>
+  <span class="icon-bar"></span>
+  <span class="icon-bar"></span>
+  </button>
+  <a href="/" title="Smallwood+Nickle" class="sn">SN</a>
+  <nav class="navbar-collapse collapse" role="navigation">
+    <?php print $footer_nav; ?>
+  </nav>
+</nav>
 
 <?php if (!empty($page['highlighted'])): ?>
   <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
